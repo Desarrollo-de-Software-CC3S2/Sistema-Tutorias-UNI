b783709e8dc14e05081cb368923e2e6a1d4ce18d
wensespl <wpenadillol@uni.pe>
2021-10-04 10:47:53 -0500

    inicio del proyecto
    Se elimino algunos archivos que no son útiles y se creo nuevas carpetas y archivos para definir la nueva estructura del proyecto.

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,38 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  :root{
+    /* colors */
+    --dark-bg: #262626;
+    --gray-1: #BCB4B4;
+    --deep-dark: #1E1E1E;
+    --gray-2: #363636;
+    --white : white;
+    --black: black;
+  }
+  html{
+    font-size: 12px;
+    font-family: 'Roboto Mono';
+    background-color: var(--white);
+  }
+  ul,li{
+    list-style: none;
+  }
+  a{
+    text-decoration: none;
+  }
+  img, svg{
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  button{
+    outline: none
+  }
+`;
+export default GlobalStyles;
